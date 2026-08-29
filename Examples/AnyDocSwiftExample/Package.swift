d6d4c45f31189f16d20a9ee29e62fcda3bd0eb6a
ngutech21 <ngutech21@pm.me>
2026-08-29 09:09:43 +0200

    chore: bump example to 0.1.4

--- Examples/AnyDocSwiftExample/Package.swift
+++ Examples/AnyDocSwiftExample/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,7 +11,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/ngutech21/anydoc-swift.git",
-      exact: "0.1.3"
+      exact: "0.1.4"
     )
   ],
   targets: [
--- Examples/AnyDocSwiftExample/Sources/AnyDocSwiftExample/AnyDocSwiftExample.swift
+++ Examples/AnyDocSwiftExample/Sources/AnyDocSwiftExample/AnyDocSwiftExample.swift
@@ -3,21 +3,20 @@ import Foundation
 
 @main
 struct AnyDocSwiftExample {
-    static func main() async throws {
-        guard let path = CommandLine.arguments.dropFirst().first else {
-            print("Usage: swift run AnyDocSwiftExample <document>")
-            return
-        }
-
-        let url = URL(fileURLWithPath: path)
-        let data = try Data(contentsOf: url)
-
-        let converter = AnyDocConverter()
-        let markdown = try await converter.markdown(
-            from: data,
-            fileExtension: url.pathExtension
-        )
-        print(markdown)
-
+  static func main() async throws {
+    guard let path = CommandLine.arguments.dropFirst().first else {
+      print("Usage: swift run AnyDocSwiftExample <document>")
+      return
     }
+
+    let url = URL(fileURLWithPath: path)
+    let data = try Data(contentsOf: url)
+
+    let converter = AnyDocConverter()
+    let markdown = try await converter.markdown(
+      from: data,
+      fileExtension: url.pathExtension
+    )
+    print(markdown)
+  }
 }
